de8925a3575b0e13444e2ae81ed2b1df2b78ad4f
randomwebdev <44571530+Kristopp@users.noreply.github.com>
2023-03-23 19:30:29 +0200

    Inital layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,11 +7,13 @@ interface Props {
 }
 
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children }: Props): JSX.Element {
     return (
         <>
             <div className='bg-background-primary'>
+                <div className='container mx-auto sm:min-w-340 md:max-w-1600 bg-background-section '>
                 <main>{children}</main>
+                </div>
 
             </div>
         </>
